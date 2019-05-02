3e28dbe986659524104a310ed44b9f13ef1a17e3
Aviv Eyal <aviveyal07@gmail.com>
2019-05-02 17:35:56 +0300

    formatting

--- contracts/tst.sol
+++ contracts/tst.sol
@@ -5,7 +5,6 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/payment/PullPayment.sol";
-
 contract TeamSpacemeshToken is ERC20Mintable, ERC20Detailed, PullPayment {
     constructor() ERC20Detailed("Team Spacemesh Token", "TST", 0) public {
         _mint(msg.sender, 1000);
