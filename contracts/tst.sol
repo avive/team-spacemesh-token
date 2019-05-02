07bd1d12f05f402500a74e013f2dd85deab5f7ca
Aviv Eyal <aviveyal07@gmail.com>
2019-05-02 17:44:26 +0300

    Cleanup soliditiy imports

--- contracts/tst.sol
+++ contracts/tst.sol
@@ -2,10 +2,7 @@
 pragma solidity >=0.5.0 <0.6.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/payment/PullPayment.sol";
-contract TeamSpacemeshToken is ERC20Mintable, ERC20Detailed, PullPayment {
+contract TeamSpacemeshToken is ERC20Mintable, ERC20Detailed {
     constructor() ERC20Detailed("Team Spacemesh Token", "TST", 0) public {
         _mint(msg.sender, 1000);
     }
