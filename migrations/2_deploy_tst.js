1dc92f25bdfc4460069957f0f770f74eca88b2c6
Aviv Eyal <aviveyal07@gmail.com>
2019-05-03 07:05:45 +0300

    Deployment config

--- migrations/2_deploy_tst.js
+++ migrations/2_deploy_tst.js
@@ -1,4 +1,5 @@
 const TeamSpacemeshToken = artifacts.require("TeamSpacemeshToken");
+TeamSpacemeshToken.synchronization_timeout = 3000;
 
 module.exports = function(deployer) {
   deployer.deploy(TeamSpacemeshToken);
